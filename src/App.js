885a51362ebb10128bf266bf9e910aedac4c9a4c
Srinivas-dev995 <elakotisrinu48@gmail.com>
2024-02-09 03:39:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
 import { useState } from "react";
 import ButtonsContainer from "./components/ButtonssContainer";
 import Display from "./components/Display";
-import './components/Display.modular.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./components/Display.modular.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
-  const [calVal,setCalVal] = useState("");
+  const [calVal, setCalVal] = useState("");
 
   const onButtonClick = (button) => {
-      if (button === "C"){
-        setCalVal("");
-
-      }else if(button === "="){
-        const result = eval(calVal);
-        setCalVal(result);
-        
-      }
-      else{
-        const newDisplayValue = calVal + button;
-        setCalVal(newDisplayValue);
-      }
-
-  }
+    if (button === "C") {
+      setCalVal("");
+    } else if (button === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + button;
+      setCalVal(newDisplayValue);
+    }
+  };
 
   return (
     <center>
-    <div className='calculator'>
-    <Display  displayValue = {calVal} />
-    <ButtonsContainer  onButtonClick={onButtonClick} />
-    </div>
+      <div className="calculator">
+        <Display displayValue={calVal} />
+        <ButtonsContainer onButtonClick={onButtonClick} />
+      </div>
     </center>
   );
 }
